d372e44118596d4905ab6df45040ee634f85cb2a
Lukasz Byszewski <lukasz.byszewski@gmail.com>
2022-09-07 07:22:26 +0200

    Modifiaction

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
 
+    //1. deklaracja zmiennej
+
+    //2. inicjalizacja zmiennej
+
+    //3. typy zmiennych
+
+    //4. operatory arytmetyczne i logiczne
+
+    //5. klasa Scanner i wyprowadzanie wyników w konsoli
+
+    //6. Zadanie do realizacji na zajęciach
+
+    //7. Zadanie do realizacji w domu
+
+    
+
+
 
 
 
