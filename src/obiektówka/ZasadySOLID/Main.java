a23490235cf1245cd820cbd757d9d32db36ede8d
3TPA <3TPA@wp.pl>
2022-09-28 07:30:28 +0200

    SOLID

---
+++ src/obiektówka/ZasadySOLID/Main.java
@@ -0,0 +1,11 @@
+package obiektówka.ZasadySOLID;
+
+public class Main {
+    public static void main(String[] args) {
+        // S - Single Responsibility Principle - jedna klasa do wyższych instancji
+        // O - Open/Close Principles - otwartość na rozszerzenia / brak możliwości zmiany
+        // L - Liskov Substitution principle - wykorzystanie funkcji klas bazowych dla pochodnych z przemyśleniem
+        // I - Iterfaces Segregation Principle - tworzenie wielu interfejsów
+        // D - Dependency Inversion Principle - klasy nieskopozimowe nie mogą być uzależnione od wyskopoziomowych
+    }
+}
